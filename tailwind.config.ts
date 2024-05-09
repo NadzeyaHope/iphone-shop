d1ff4c06ef13946dd12d098f74d6ed29e8e7b557
NadzeyaHope <radkovanada13@gmail.com>
2024-05-09 16:00:08 +0300

    add themes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,46 @@
 import type { Config } from "tailwindcss";
+import { nextui } from '@nextui-org/react';
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}" ,
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui({
+    layout: {
+      disabledOpacity: "0.3", // opacity-[0.3]
+      radius: {
+        small: "2px", // rounded-small
+        medium: "4px", // rounded-medium
+        large: "6px", // rounded-large
+      },
+      borderWidth: {
+        small: "1px", // border-small
+        medium: "1px", // border-medium
+        large: "2px", // border-large
+      },
+    },
+    themes: {
+      light: {
+        colors : {
+          background : '#ededed',
+        },
+      },
+      dark: {
+        colors : {
+          background : '#272727',
+        },
+      },
+    },
+  }),
+  ],
 };
 export default config;
