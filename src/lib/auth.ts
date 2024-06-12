acab5a9e65c1c84b8d362392d10b345ea094b3d6
NadzeyaHope <radkovanada13@gmail.com>
2024-06-12 13:57:09 +0300

    add small

---
+++ src/lib/auth.ts
@@ -0,0 +1,19 @@
+// pages/api/auth/[...nextauth].js
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import EmailProvider from "next-auth/providers/email"
+
+export default NextAuth({
+    secret: process.env.SECRET,
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET,
+        }),
+        // Sign in with passwordless email link
+        EmailProvider({
+            server: process.env.MAIL_SERVER,
+            from: "<no-reply@example.com>",
+        }),
+    ],
+})
--- src/widgets/auth/ButtonGoogleSignup.tsx
+++ src/widgets/auth/ButtonGoogleSignup.tsx
@@ -3,9 +3,13 @@ import GoogleIcon from "../../../public/authIcons/GoogleIcon";
 import {Button} from "@nextui-org/react";
 
 const ButtonGoogleSignup = () => {
+    const loginWithGoogle = () => {
+
+    }
+
     return (
         <Button
-
+            onClick={loginWithGoogle}
             className={'rounded-xl border-1 text-center'}><GoogleIcon/> Sign up with Google</Button>
     );
 };
