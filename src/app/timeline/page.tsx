46fdfdd5f577223f820ee48a0089d3840dd1a57a
NadzeyaHope <radkovanada13@gmail.com>
2024-06-14 23:05:22 +0300

    add in mongodb data

--- src/app/timeline/page.tsx
+++ src/app/timeline/page.tsx
@@ -1,6 +1,7 @@
 import { getServerSession } from "next-auth";
 import authConfig from "../../lib/auth";
 import {redirect} from "next/navigation";
+import {Image} from "@nextui-org/react";
 
 const wait = (ms: number) => new Promise((rs) => setTimeout(rs, ms));
 
@@ -16,8 +17,26 @@ export default async function Page() {
 
     await wait(1000);
 
+
+    if (session?.user) {
+        const response = await fetch('http://localhost:3000/api/users', {
+            method: 'POST',
+
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: session.user.email,
+                name: session.user.name,
+                image: session.user.image,
+            }),
+        });
+    }
+
+    console.log(`${session?.user?.email} this user`);
     return (
         <>
+            <Image src={String(session?.user?.image)}  width={100} height={100} />
             <h3>This is your timeline: {session?.user?.email}</h3>
             <h3>This is your timeline: {session?.user?.name}</h3>
         </>
